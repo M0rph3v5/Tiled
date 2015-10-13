52e32387aafdc8e8c654b4dc249841b5f2597248
Benjamin de Jager <m0rph3v5@gmail.com>
2015-10-13 19:02:23 +0200

    .gitignore is now working

--- examples/iOSLocalTilesExample/Pods/Target Support Files/Tiled/Tiled-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Tiled : NSObject
-@end
-@implementation PodsDummy_Tiled
-@end
--- examples/iOSLocalTilesExample/Pods/Target Support Files/Tiled/Tiled-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double TiledVersionNumber;
-FOUNDATION_EXPORT const unsigned char TiledVersionString[];
-
